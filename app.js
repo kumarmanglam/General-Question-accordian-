0070bca959bc15269df6165dca7d316bfe99e8cd
Kumar Manglam <kumarthe799@gmail.com>
2023-03-23 18:20:24 +0530

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+    question.classList.toggle("show-text");
+  });
+});
